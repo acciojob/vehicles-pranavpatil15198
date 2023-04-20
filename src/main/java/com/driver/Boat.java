82163d76ee22da6b1361b1a0f3ba1460406626c9
Pranav Patil <121039421+pranavpatil15198@users.noreply.github.com>
2023-04-20 14:08:30 +0530

    Create Boat.java

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,28 @@
+package com.driver;
+
+public class Boat implements WaterVehicle
+{
+    String name;
+    int capacity;
+    @Override
+    public String getVehicleName()
+    {
+        return this.name;
+    }
+
+    public Boat()
+    {
+    }
+
+    public Boat(String name, int capacity)
+    {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public int getVehicleCapacity()
+    {
+        return this.capacity;
+    }
+}
